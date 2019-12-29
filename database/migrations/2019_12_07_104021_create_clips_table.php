a68f802977b651d31193b122a18ac946d688a6b7
DeGraciaMathieu <contact@degracia-mathieu.fr>
2019-12-29 20:23:31 +0100

    Ajout des migrations clips & curators

---
+++ database/migrations/2019_12_07_104021_create_clips_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClipsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clips', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('curator_id');
+            $table->unsignedBigInteger('tracking_id')->unique();
+            $table->string('slug');
+            $table->string('game')->nullable();
+            $table->string('title')->nullable();
+            $table->string('thumbnail')->nullable();
+            $table->string('url')->unique();
+            $table->boolean('active')->default(false);
+            $table->timestamp('approved_at')->nullable();            
+            $table->timestamps();
+
+            $table->foreign('curator_id')->references('id')->on('curators');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clips');
+    }
+}
---
+++ database/migrations/2019_12_07_105143_create_curators_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCuratorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('curators', function (Blueprint $table) {
+            $table->unsignedBigInteger('id');
+            $table->unsignedBigInteger('tracking_id')->unique();
+            $table->string('name');
+            $table->string('display_name');
+            $table->string('channel_url');
+            $table->string('logo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clips');
+        Schema::dropIfExists('curators');
+    }
+}
