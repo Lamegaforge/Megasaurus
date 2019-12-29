5dc63750cbe68f670b19d3cd5f2d8e1f341554ee
DeGraciaMathieu <contact@degracia-mathieu.fr>
2019-12-29 20:12:12 +0100

    Création du ClipService

---
+++ app/Services/ClipService.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Services;
+
+use App\Clip;
+use App\Curator;
+use App\Repositories\ClipRepository;
+
+class ClipService
+{
+    protected $clipRepository;
+
+    public function __construct(ClipRepository $clipRepository)
+    {
+        $this->clipRepository = $clipRepository;
+    }
+
+    public function alreadySave(string $trackingId) :bool
+    {
+        $clip = $this->clipRepository->findByField('tracking_id', $trackingId);
+
+        return (bool) $clip;
+    }
+
+    public function store(array $attributes, Curator $curator) :Clip
+    {
+        $attributes['curator_id'] = $curator->id;
+
+        $clip = $this->clipRepository->create($attributes);
+
+        return $clip;
+    }
+}
